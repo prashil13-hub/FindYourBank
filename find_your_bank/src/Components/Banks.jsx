4e3f993024e35e09ca6404cd4a6fb93fae1a0d2e
Prashil13 <prashilganvir13@gmail.com>
2022-05-06 23:35:22 +0530

    added pagination to add banks page

--- find_your_bank/src/Components/Banks.jsx
+++ find_your_bank/src/Components/Banks.jsx
@@ -12,9 +12,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import Stack from '@mui/material/Stack';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 export default function Banks() {
 
-    const[allBanks,setAllBanks] = useState([]);
+    let[allBanks,setAllBanks] = useState([]);
+    const[currPage,setCurrPage] = useState(1)
 
     // useEffect(async()=>{
     //     const res = await axios.get(`https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI`)
@@ -29,6 +36,23 @@ export default function Banks() {
             setAllBanks(res.data);
         })();
     }, []);
+
+    const handlePageChnage=(item)=>{
+        console.log(item)
+        setCurrPage(item.page)
+        item.selected=true
+    }
+
+    let pages = Math.ceil(allBanks.length/10);
+    let pageArr = [];
+    for(let i=1;i<=pages;i++){
+        pageArr.push(i);
+    }
+    // console.log(pageArr)
+    let si = (currPage-1)*10;
+    let ei = si+10;
+    let filterBanks=[]
+    filterBanks=allBanks.slice(si,ei);
 
     return (
         <div style={{margin:'25px'}}>
@@ -45,7 +69,7 @@ export default function Banks() {
                     </TableRow>
                     </TableHead>
                     <TableBody>
-                    {allBanks.map((bank) => (
+                    {filterBanks.map((bank) => (
                         <TableRow
                         key={uuidv4()}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -62,7 +86,21 @@ export default function Banks() {
                     </TableBody>
                 </Table>
             </TableContainer>
-            
+
+            <Stack spacing={2} style={{display:'flex',alignItems:'center',marginTop:'10px'}}>
+                <Pagination
+                    count={pages}
+                    renderItem={(item) => (
+                    <PaginationItem
+                        components={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+                        {...item}
+                        onClick={()=>handlePageChnage(item)}
+                    />
+                    )}
+                    
+                />
+            </Stack>
+
         </div>
     )
 }
