b915dc440a520a324426da1117ce7845abe6917a
Prashil13 <prashilganvir13@gmail.com>
2022-05-06 20:13:03 +0530

    started with pagination

--- find_your_bank/src/Components/Banks.jsx
+++ find_your_bank/src/Components/Banks.jsx
@@ -31,17 +31,17 @@ export default function Banks() {
     }, []);
 
     return (
-        <div>
+        <div style={{margin:'25px'}}>
 
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Bank</TableCell>
+                        <TableCell align="right">IFSC</TableCell>
+                        <TableCell align="right">Branch</TableCell>
+                        <TableCell align="right">Bank ID</TableCell>
+                        <TableCell align="right">Adress&nbsp;</TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
